e3d7a15e21df05614f2c706f25e9f10f0955eab0
Darshan <darshanp@vayavyalabs.com>
2025-04-28 13:01:15 +0530

    FEAT: Added more functions for handling different requests

--- dusb.c
+++ dusb.c
@@ -23,19 +23,46 @@ OBJECT_DECLARE_SIMPLE_TYPE(DUSBState, USB_DUSB)
 /* Device state */
 typedef struct DUSBState {
     USBDevice dev;
-    USBEndpoint *ep_int;
-    USBEndpoint *ep_bulk_in, *ep_bulk_out;
-    USBEndpoint *ep_iso;
     uint8_t alt[3];
-    uint8_t data[64];
+    USBEndpoint *ep_int_out, *ep_int_in;
+    USBEndpoint *ep_iso_out, *ep_iso_in;
+    USBEndpoint *ep_bulk_out, *ep_bulk_in;
 } DUSBState;
 
-/* Endpoint descriptors (non-const to match eps pointer type) */
-static USBDescEndpoint ep_desc_int = {
+/* Endpoint descriptors */
+static USBDescEndpoint ep_desc_int_out = {
+    .bEndpointAddress = USB_DIR_OUT | 1,
+    .bmAttributes     = USB_ENDPOINT_XFER_INT,
+    .wMaxPacketSize   = 64,
+    .bInterval        = 1,
+};
+
+static USBDescEndpoint ep_desc_int_in = {
     .bEndpointAddress = USB_DIR_IN | 1,
     .bmAttributes     = USB_ENDPOINT_XFER_INT,
     .wMaxPacketSize   = 64,
     .bInterval        = 1,
+};
+
+static USBDescEndpoint ep_desc_iso_out = {
+    .bEndpointAddress = USB_DIR_OUT | 2,
+    .bmAttributes     = USB_ENDPOINT_XFER_ISOC,
+    .wMaxPacketSize   = 64,
+    .bInterval        = 1,
+};
+
+static USBDescEndpoint ep_desc_iso_in = {
+    .bEndpointAddress = USB_DIR_IN | 2,
+    .bmAttributes     = USB_ENDPOINT_XFER_ISOC,
+    .wMaxPacketSize   = 64,
+    .bInterval        = 1,
+};
+
+static USBDescEndpoint ep_desc_bulk_out = {
+    .bEndpointAddress = USB_DIR_OUT | 3,
+    .bmAttributes     = USB_ENDPOINT_XFER_BULK,
+    .wMaxPacketSize   = 64,
+    .bInterval        = 0,
 };
 
 static USBDescEndpoint ep_desc_bulk_in = {
@@ -44,67 +71,56 @@ static USBDescEndpoint ep_desc_bulk_in = {
     .wMaxPacketSize   = 64,
     .bInterval        = 0,
 };
-static USBDescEndpoint ep_desc_bulk_out = {
-    .bEndpointAddress = USB_DIR_OUT | 3,
-    .bmAttributes     = USB_ENDPOINT_XFER_BULK,
-    .wMaxPacketSize   = 64,
-    .bInterval        = 0,
-};
-
-static USBDescEndpoint ep_desc_iso = {
-    .bEndpointAddress = USB_DIR_IN | 5,
-    .bmAttributes     = USB_ENDPOINT_XFER_ISOC,
-    .wMaxPacketSize   = 64,
-    .bInterval        = 1,
-};
 
 /* Interfaces with alternate settings */
 static const USBDescIface ifaces[] = {
-    /* Interrupt: alt 0 (disabled) */
+    /* Interface 0, alt 0: Interrupt OUT */
     {
         .bInterfaceNumber    = 0,
         .bAlternateSetting   = 0,
-        .bNumEndpoints       = 0,
+        .bNumEndpoints       = 1,
         .bInterfaceClass     = 0xFF,
+        .eps                 = &ep_desc_int_out,
     },
-    /* Interrupt: alt 1 (IN endpoint) */
+    /* Interface 0, alt 1: Interrupt IN */
     {
         .bInterfaceNumber    = 0,
         .bAlternateSetting   = 1,
         .bNumEndpoints       = 1,
         .bInterfaceClass     = 0xFF,
-        .eps                 = &ep_desc_int,
+        .eps                 = &ep_desc_int_in,
     },
-    /* Bulk: alt 0 (OUT) */
+    /* Interface 1, alt 0: Isochronous OUT */
     {
         .bInterfaceNumber    = 1,
+        .bAlternateSetting   = 0,
+        .bNumEndpoints       = 1,
+        .bInterfaceClass     = 0xFF,
+        .eps                 = &ep_desc_iso_out,
+    },
+    /* Interface 1, alt 1: Isochronous IN */
+    {
+        .bInterfaceNumber    = 1,
+        .bAlternateSetting   = 1,
+        .bNumEndpoints       = 1,
+        .bInterfaceClass     = 0xFF,
+        .eps                 = &ep_desc_iso_in,
+    },
+    /* Interface 2, alt 0: Bulk OUT */
+    {
+        .bInterfaceNumber    = 2,
         .bAlternateSetting   = 0,
         .bNumEndpoints       = 1,
         .bInterfaceClass     = 0xFF,
         .eps                 = &ep_desc_bulk_out,
     },
-    /* Bulk: alt 1 (IN) */
+    /* Interface 2, alt 1: Bulk IN */
     {
-        .bInterfaceNumber    = 1,
+        .bInterfaceNumber    = 2,
         .bAlternateSetting   = 1,
         .bNumEndpoints       = 1,
         .bInterfaceClass     = 0xFF,
         .eps                 = &ep_desc_bulk_in,
-    },
-    /* Isochronous: alt 0 (disabled) */
-    {
-        .bInterfaceNumber    = 2,
-        .bAlternateSetting   = 0,
-        .bNumEndpoints       = 0,
-        .bInterfaceClass     = 0xFF,
-    },
-    /* Isochronous: alt 1 (IN) */
-    {
-        .bInterfaceNumber    = 2,
-        .bAlternateSetting   = 1,
-        .bNumEndpoints       = 1,
-        .bInterfaceClass     = 0xFF,
-        .eps                 = &ep_desc_iso,
     },
 };
 
@@ -141,20 +157,13 @@ static const USBDesc desc = {
     .str  = (USBDescStrings){ "", "Darshan", "DUSB Device", "69-420" },
 };
 
-/* Initialize sample data pattern */
-static void dusb_init_data(DUSBState *s)
-{
-    for (int i = 0; i < sizeof(s->data); i++) {
-        s->data[i] = (uint8_t)i;
-    }
-}
-
 /* Handle control requests (SET_INTERFACE) */
 static void dusb_handle_control(USBDevice *dev, USBPacket *p,
                                 int request, int value,
                                 int index, int length,
                                 uint8_t *data)
 {
+    qemu_log("DUSB: Handling control request: 0x%04x\n", request);
     if (usb_desc_handle_control(dev, p, request, value, index, length, data) >= 0) {
         return;
     }
@@ -165,6 +174,7 @@ static void dusb_handle_control(USBDevice *dev, USBPacket *p,
         int alt   = value;
         if (iface < 3 && alt < 2) {
             s->alt[iface] = alt;
+            qemu_log("DUSB: Set interface %d to alternate setting %d\n", iface, alt);
         }
         p->status = USB_RET_SUCCESS;
         break;
@@ -174,14 +184,47 @@ static void dusb_handle_control(USBDevice *dev, USBPacket *p,
     }
 }
 
-/* Handle transfer (just echo data) */
+/* Handle transfer */
 static void dusb_handle_data(USBDevice *dev, USBPacket *p)
 {
-    DUSBState *s = DO_UPCAST(DUSBState, dev, dev);
-    int size = p->iov.size;
-    usb_packet_copy(p, s->data, size);
-    qemu_log("DUSB: Got Data %s\n", s->data);
+    USBEndpoint *ep = p->ep;
+    int ep_num = ep->nr;
+    bool is_in = ep->pid == USB_TOKEN_IN;
+    qemu_log("DUSB: Handling data for endpoint %d %s\n", ep_num, is_in ? "IN" : "OUT");
+
+    if (is_in) {
+        // IN transfer: send static data
+        uint8_t data[64];
+        data[0] = ep_num;
+        memset(data + 1, ep_num * 0x11, sizeof(data) - 1);
+        usb_packet_copy(p, data, sizeof(data));
+    } else {
+        // OUT transfer: receive and log data
+        uint8_t buf[64];
+        int len = p->iov.size;
+        usb_packet_copy(p, buf, len);
+        if (len > 0) {
+            qemu_log("DUSB: Received %d bytes on endpoint %d OUT: %02x %02x %02x...\n",
+                     len, ep_num, buf[0], buf[1], len > 2 ? buf[2] : 0);
+        } else {
+            qemu_log("DUSB: Received 0 bytes on endpoint %d OUT\n", ep_num);
+        }
+    }
     usb_packet_complete(dev, p);
+}
+
+/* Flush endpoint queue */
+static void dusb_flush_ep_queue(USBDevice *dev, USBEndpoint *ep)
+{
+    qemu_log("DUSB: Flushing endpoint queue for endpoint %d\n", ep->nr);
+    // Implementation if needed
+}
+
+/* Endpoint stopped */
+static void dusb_ep_stopped(USBDevice *dev, USBEndpoint *ep)
+{
+    qemu_log("DUSB: Endpoint %d stopped\n", ep->nr);
+    // Implementation if needed
 }
 
 /* Reset handler */
@@ -189,8 +232,7 @@ static void dusb_handle_reset(USBDevice *dev)
 {
     DUSBState *s = DO_UPCAST(DUSBState, dev, dev);
     memset(s->alt, 0, sizeof(s->alt));
-    qemu_log("DUSB: Resetting Device %X\n",s->alt);
-    dusb_init_data(s);
+    qemu_log("DUSB: Resetting Device\n");
 }
 
 /* Realize (device init) */
@@ -200,13 +242,14 @@ static void dusb_realize(USBDevice *dev, Error **errp)
     dev->usb_desc = &desc;
     usb_desc_create_serial(dev);
     usb_desc_init(dev);
-    dusb_init_data(s);
 
-    /* Retrieve endpoints */
-    s->ep_int      = usb_ep_get(dev, USB_TOKEN_IN, 1);
+    // Retrieve endpoints
+    s->ep_int_out = usb_ep_get(dev, USB_TOKEN_OUT, 1);
+    s->ep_int_in = usb_ep_get(dev, USB_TOKEN_IN, 1);
+    s->ep_iso_out = usb_ep_get(dev, USB_TOKEN_OUT, 2);
+    s->ep_iso_in = usb_ep_get(dev, USB_TOKEN_IN, 2);
     s->ep_bulk_out = usb_ep_get(dev, USB_TOKEN_OUT, 3);
-    s->ep_bulk_in  = usb_ep_get(dev, USB_TOKEN_IN, 3);
-    s->ep_iso      = usb_ep_get(dev, USB_TOKEN_IN, 5);
+    s->ep_bulk_in = usb_ep_get(dev, USB_TOKEN_IN, 3);
 }
 
 /* Class init wiring */
@@ -217,6 +260,8 @@ static void dusb_class_init(ObjectClass *klass, void *data)
     k->handle_control = dusb_handle_control;
     k->handle_data    = dusb_handle_data;
     k->handle_reset   = dusb_handle_reset;
+    k->flush_ep_queue = dusb_flush_ep_queue;
+    k->ep_stopped     = dusb_ep_stopped;
     k->product_desc   = "Custom DUSB";
 }
 
@@ -232,5 +277,6 @@ static void dusb_register(void)
 {
     type_register_static(&dusb_info);
 }
-
+#ifdef CONFIG_DUSB
 type_init(dusb_register);
+#endif
